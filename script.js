b4934cf4ffa9f7c65a30dd0a5821a35f20ae8bb6
Marilise Morona <marymorona@gmail.com>
2021-01-22 15:41:40 -0300

    correções do code

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
-const dino = document.querySelector('dino');
+const dino = document.querySelector('.dino');
 //na forma de cont não permite subscrever. se a opção for let dno .. ai pemite sobescrever a variável dino
-const background = document.querySelector('backgroud');
+const background = document.querySelector('.background');
+
 let isJumping = false;
 let isGameOver = false;
 let position = 0;
 
-function handleKeyUp(event){
-    if (event.keyCode === 32){
+function handleKeyUp(event) {
+    if(event.keyCode === 32){
 // 32 é e keyCode ou valor, referente a tecla espaço, ou seja o dino só vai pular se apertar a tecla espaço
-        if (!isJumping){
+        if(!isJumping){
             jump();
 // se ele não tiver pulando, ele pula (evita bug)
         }
     }
 }
-function jump (){
+function jump(){
     isJumping = true;
  let upInterval = setInterval(() =>{
-    if (position >=150){
+    if(position >=150){
         clearInterval(upInterval);
 
         let downInterval = setInterval(() =>{
-            if (position <= 0){
+            if(position <= 0){
                 clearInterval(downInterval);
                 isJumping = false;
             }else {
-                position -=20;
+                position -= 20;
                 dino.style.bottom = position + 'px';
             }
         }, 20);
     } else {
         position +=20;
-        dino.style.bottom = position + 'px'
+        dino.style.bottom = position + 'px';
     }
 }, 20);
 }
-function createCactus(){
+function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
     let randomTime = Math.random() * 6000;
 
-    if (isGameOver) return;
-
+    if(isGameOver)return;
+    
     cactus.classList.add('cactus');
     background.appendChild(cactus);
-    cactus.style.left = cactusPosition + 'px'
+    cactus.style.left = cactusPosition + 'px';
     
-    let leftTimer = setInterval(() => {
-              
-        if (cactusPosition < -60){
+    let leftTimer=setInterval(() => {
+            if(cactusPosition < -60){
             clearInterval(leftTimer);
             background.removeChild(cactus);
         }
-        else if (cactusPosition > 0 && cactusPosition < 60){
-            clearInterval(leftInterval);
+        else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            clearInterval(leftTimer);
             isGameOver = true;
 //Quando o cactus atingir a posição do dino = game over
-            document.body.innerHTML = 'h1 class="game-over">Fim de jogo</h1>';
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
         }
         else {
             cactusPosition -= 10;
-            cactus.style.left = cactusPosition + 'px'
+            cactus.style.left = cactusPosition + 'px';
         }
 
     }, 20);
